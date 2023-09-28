0bd768ed0ec65514c450a236fa6a9bfefaf345a7
User <huseynovali612@gmail.com>
2023-09-28 22:54:28 +0400

    ali change

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,8 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
-export default function App() {
+function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -33,4 +32,4 @@ export default function App() {
   )
 }
 
-
+export default App
